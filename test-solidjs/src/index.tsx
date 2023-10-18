2aff079161c47497b2905857192d23cc8808d07e
Diego Vila <diego@velascommerce.com>
2023-10-17 20:24:29 -0400

    got link and webcomponents to work

--- test-solidjs/src/index.tsx
+++ test-solidjs/src/index.tsx
@@ -1,5 +1,7 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
+import  { defineCustomElement } from 'test-stencil/dist/components/my-component'
+defineCustomElement();
 
 import './index.css'
 import App from './App'
