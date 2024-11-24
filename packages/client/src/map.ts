d13f0318507cc2fdc0afd32b95e7a64e270e6048
Diego R Vila <dego_vila@yahoo.com>
2024-11-23 21:11:59 -0400

    feat: web component decorator

--- packages/client/src/map.ts
+++ packages/client/src/map.ts
@@ -1,9 +1,15 @@
 import '@utils/map/here_wego';
-
+import { HereWeGO } from '@utils/map/here_wego';
+import { $ } from '@utils/dom';
 
 setTimeout(() => {
-  let $elem = document.querySelector('here-wego')
-  console.log($elem);
-  $elem?.setAttribute("test", "here-wego after 2 seconds")
+  let $elem = $('here-wego');
+  $elem.set("test", "here-wego after 2 seconds");
 }, 2000);
+
+setTimeout(() => {
+  let $elem = $('here-wego');
+  let elem = $elem[0] as HereWeGO;
+  elem.test = "here-wego after 4 seconds"
+}, 4000);
 
