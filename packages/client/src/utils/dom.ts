8f6688854ec8814425874a7357ae033ba210da73
Diego R Vila <dego_vila@yahoo.com>
2024-11-24 23:30:11 -0400

    refactor: here

--- packages/client/src/utils/dom.ts
+++ packages/client/src/utils/dom.ts
@@ -1,28 +1,46 @@
 
+class $Elements {
+  private elements: Array<Element>
 
-class $Element {
-  constructor(private elem: Element) { }
+  constructor(elems: NodeListOf<Element>) {
+    this.elements = Array.from(elems);
+  }
 
   set(prop: string, value: string) {
-    this.elem.setAttribute(prop, value);
+    this.elements.forEach((elem) => {
+      elem.setAttribute(prop, value);
+    })
   }
 
   get(prop) {
+    // if prop get attributes
     if (isNaN(Number(prop))) {
-      return this.elem.getAttribute(prop)
+      const props = this.elements.map((elem) => {
+        return elem.getAttribute(prop);
+      });
+
+      if (props.length == 1) {
+        return props[0]
+      }
+      else {
+        return props
+      }
     }
+    // if index get element
     else {
-      return this.elem;
+      return this.elements[prop];
     }
   }
 }
 
 export function $(query: string) {
-  let proxy = new Proxy(new $Element(document.querySelector(query)!), {
+  let proxy = new Proxy(new $Elements(document.querySelectorAll(query)), {
     get(obj, prop) {
+      // if prop is not a number
       if (isNaN(Number(prop)) == true) {
         return obj[prop];
       }
+      // prop is an index
       else {
         return obj.get(prop)
       }
